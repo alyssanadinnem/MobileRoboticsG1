11e624f9dd8459973c108d11af9af60a25f8d06e
JBee266 <jennifer.bura2@mail.dcu.ie>
2025-02-18 16:39:58 +0000

    esp info added

--- MobileRoboticsGroup1/MobileRoboticsGroup1.ino
+++ MobileRoboticsGroup1/MobileRoboticsGroup1.ino
@@ -1,6 +1,8 @@
 #include <esp_now.h>
 #include <WiFi.h>
 
+//"kabikis75windfall" - MAC: - old esp wifi - Channel: 11
+//"needlings84wheezily" - MAC: 48:ca:43:06:13:fc new esp wifi - Channel: 11
 
 // Structure example to receive data
 // Must match the sender structure
@@ -24,12 +26,12 @@ void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
  
 void setup() {
   // Initialize Serial Monitor
-  Serial.begin(9600);
+  Serial.begin(115200);
   
   // Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
 
-  // Init ESP-NOW
+  // Initialize ESP-NOW
   if (esp_now_init() != ESP_OK) {
     Serial.println("Error initializing ESP-NOW");
     return;
@@ -40,4 +42,4 @@ void setup() {
  
 void loop() {
 
-}
+  }
